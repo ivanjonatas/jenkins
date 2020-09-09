858723882841ab63f26ad5922748d2ff993b0778
ivan.jonatas <ivan.jonatas@conductor.com.br>
2020-09-09 18:37:53 -0300

    adicionando comentario no arquivo de instalação do ubuntu

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -1,6 +1,11 @@
+# após baixar o arquivo entre no diretorio,conceda permissão ao arquivo e execute.
+# chmod +x ubuntu.sh
+# ./ubuntu.sh
+
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install jenkins -y
 sudo systemctl start jenkins
+#CHAVE JENKINS
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
